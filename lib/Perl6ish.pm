59e3eccf4e9720b8643bfb259facc2c76955af71
Liu Kang-min <gugod@gugod.org>
2009-01-11 00:23:48 +0800

    initial commit

---
+++ lib/Perl6ish.pm
@@ -0,0 +1,33 @@
+package Perl6ish;
+
+use strict;
+use warnings;
+our $VERSION = '0.01';
+
+1;
+__END__
+
+=head1 NAME
+
+Perl6ish -
+
+=head1 SYNOPSIS
+
+  use Perl6ish;
+
+=head1 DESCRIPTION
+
+Perl6ish is
+
+=head1 AUTHOR
+
+Kang-min Liu E<lt>gugod@gugod.orgE<gt>
+
+=head1 SEE ALSO
+
+=head1 LICENSE
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=cut
